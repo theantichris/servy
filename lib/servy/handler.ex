ac1599bd8da3e5e85a87507557146bba4acfd1af
Christopher Lamm <chris@theantichris.com>
2020-04-28 07:07:39 -0400

    handling status codes

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -14,7 +14,7 @@ defmodule Servy.Handler do
             |> List.first
             |> String.split(" ")
 
-        %{ method: method, path: path, resp_body: "" }
+        %{ method: method, path: path, resp_body: "", status: nil }
     end
 
     def log(conv), do: IO.inspect conv
@@ -24,25 +24,36 @@ defmodule Servy.Handler do
     end
 
     def route(conv, "GET", "/wildthings") do
-        %{ conv | resp_body: "Bears, Lions, Tigers" }
+        %{ conv | resp_body: "Bears, Lions, Tigers", status: 200 }
     end
 
     def route(conv, "GET", "/bears") do
-        %{ conv | resp_body: "Teddy, Smokey, Paddington" }
+        %{ conv | resp_body: "Teddy, Smokey, Paddington", status: 200 }
     end
 
     def route(conv, _method, path) do
-        %{ conv | resp_body: "No #{path} here!"}
+        %{ conv | resp_body: "No #{path} here!", status: 404}
     end
 
     def format_response(conv) do
         """
-        HTTP/1.1 200 OK
+        HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
         Content-Type: text/html
         Content-Length: #{byte_size(conv.resp_body)}
 
         #{conv.resp_body}
         """
+    end
+
+    defp status_reason(code) do
+        %{
+            200 => "OK",
+            201 => "Created",
+            401 => "Unauthorized",
+            403 => "Forbidden",
+            404 => "Not Found",
+            500 => "Internal Server Error"
+        }[code]
     end
 end
 
